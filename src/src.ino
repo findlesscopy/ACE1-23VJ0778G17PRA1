86fa7981bfb315f30b926aa0e51f962ea5bf2204
findlesscopy <findlesscopy@gmail.com>
2023-06-06 21:46:31 -0600

    fix: proteus

---
+++ src/src.ino
@@ -0,0 +1,25 @@
+/*
+    Código fuente - Práctica 1 - Arquitectura de Computadores y Ensambladores 1
+    Grupo:  17
+    Integrantes:
+        -  
+        - 
+        - 
+        - José Manuel Ibarra Pirir 202001800
+        - 
+*/
+
+//Librerias que se usan
+#include <LedControl.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
